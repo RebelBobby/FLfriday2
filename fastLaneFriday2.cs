c80846a4c0863381971b158dc000158f10e9c938
RebelBobby <joseph.matulewicz@gmail.com>
2022-07-22 11:44:16 -0700

    second class

--- fastLaneFriday2.cs
+++ fastLaneFriday2.cs
@@ -8,11 +8,43 @@ namespace Nerd
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, Nerd head!");
-
-
-        
+       
 
         }
 
     }
 }
+
+
+/*-----------------------------------second class*/
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nerd
+{
+    internal class Person
+    {
+        public int Age { get; set; }
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public Person() { }
+        public Person(string firstName, string lastName, int age)
+        {
+            Age = age; FirstName = firstName; LastName = lastName;
+
+        }
+        public override string ToString()
+        {
+            return $"Name: {FirstName} {LastName}, Age: {Age}";
+        }
+    }
+
+}
+
+

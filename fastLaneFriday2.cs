37bf418789523f89874e74d2da40b3ea3bbd1352
RebelBobby <joseph.matulewicz@gmail.com>
2022-07-22 11:05:29 -0700

    fastlanefriday2

---
+++ fastLaneFriday2.cs
@@ -0,0 +1,18 @@
+
+using System;
+
+namespace Nerd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Nerd head!");
+
+
+        
+
+        }
+
+    }
+}
